725e144f59510774b9e960829c2e5d28a3a1e93f
christianpipa2026-tech <christianpipa2026@gmail.com>
2026-07-01 12:23:58 -0300

    Copiar src/curriculum_Master.js

---
+++ src/curriculum_Master.js
@@ -0,0 +1,7 @@
+import { MASTER_U1_U2 } from "./curriculum_Master_part1.js";
+import { MASTER_U3_U4 } from "./curriculum_Master_part2.js";
+import { MASTER_U5_U8 } from "./curriculum_Master_part3.js";
+
+export const CURRICULUM_MASTER = {
+  units: [...MASTER_U1_U2, ...MASTER_U3_U4, ...MASTER_U5_U8]
+};
