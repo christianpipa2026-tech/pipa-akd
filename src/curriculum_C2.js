8893d75c65a99737de05c8d4158c7a33716da932
christianpipa2026-tech <christianpipa2026@gmail.com>
2026-07-01 12:23:54 -0300

    Copiar src/curriculum_C2.js

---
+++ src/curriculum_C2.js
@@ -0,0 +1,7 @@
+import { C2_U1_U2 } from "./curriculum_C2_part1.js";
+import { C2_U3_U4 } from "./curriculum_C2_part2.js";
+import { C2_U5_U8 } from "./curriculum_C2_part3.js";
+
+export const CURRICULUM_C2 = {
+  units: [...C2_U1_U2, ...C2_U3_U4, ...C2_U5_U8]
+};
