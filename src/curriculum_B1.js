3f6eff3de66a960e675725ff463b9feedf946bb2
christianpipa2026-tech <christianpipa2026@gmail.com>
2026-07-01 12:23:43 -0300

    Copiar src/curriculum_B1.js

---
+++ src/curriculum_B1.js
@@ -0,0 +1,7 @@
+import { B1_U1_U2 } from "./curriculum_B1_part1.js";
+import { B1_U3_U4 } from "./curriculum_B1_part2.js";
+import { B1_U5_U8 } from "./curriculum_B1_part3.js";
+
+export const CURRICULUM_B1 = {
+  units: [...B1_U1_U2, ...B1_U3_U4, ...B1_U5_U8]
+};
