6b72afa943c762b9fe962a62cd6d615094b1a27c
christianpipa2026-tech <christianpipa2026@gmail.com>
2026-07-02 12:41:35 -0300

    C1 español: src/curriculum_C1.js

--- src/curriculum_C1.js
+++ src/curriculum_C1.js
@@ -1,7 +1,11 @@
-import { C1_U1_U2 } from "./curriculum_C1_part1.js";
-import { C1_U3_U4 } from "./curriculum_C1_part2.js";
-import { C1_U5_U8 } from "./curriculum_C1_part3.js";
+import { CURRICULUM_ES_C1_PART1 } from "./curriculum_C1_part1";
+import { CURRICULUM_ES_C1_PART2 } from "./curriculum_C1_part2";
+import { CURRICULUM_ES_C1_PART3 } from "./curriculum_C1_part3";
 
 export const CURRICULUM_C1 = {
-  units: [...C1_U1_U2, ...C1_U3_U4, ...C1_U5_U8]
+  units: [
+    ...CURRICULUM_ES_C1_PART1.units,
+    ...CURRICULUM_ES_C1_PART2.units,
+    ...CURRICULUM_ES_C1_PART3.units,
+  ]
 };
