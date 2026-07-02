3ae8ec8b81de38e19d3e7e302725af728f979cab
christianpipa2026-tech <christianpipa2026@gmail.com>
2026-07-02 06:30:58 -0300

    curriculum_B1.js: unir partes do B1

--- src/curriculum_B1.js
+++ src/curriculum_B1.js
@@ -1,7 +1,11 @@
-import { B1_U1_U2 } from "./curriculum_B1_part1.js";
-import { B1_U3_U4 } from "./curriculum_B1_part2.js";
-import { B1_U5_U8 } from "./curriculum_B1_part3.js";
+import { CURRICULUM_ES_B1_PART1 } from "./curriculum_B1_part1";
+import { CURRICULUM_ES_B1_PART2 } from "./curriculum_B1_part2";
+import { CURRICULUM_ES_B1_PART3 } from "./curriculum_B1_part3";
 
 export const CURRICULUM_B1 = {
-  units: [...B1_U1_U2, ...B1_U3_U4, ...B1_U5_U8]
+  units: [
+    ...CURRICULUM_ES_B1_PART1.units,
+    ...CURRICULUM_ES_B1_PART2.units,
+    ...CURRICULUM_ES_B1_PART3.units,
+  ]
 };
